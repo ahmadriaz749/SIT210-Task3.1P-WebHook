2cb68f0cb2b73756d6f9c9882e5ddbfa1b12c278
ahmadriaz749 <82609261+ahmadriaz749@users.noreply.github.com>
2021-04-29 02:21:04 +1000

    Add files via upload

---
+++ light.ino
@@ -0,0 +1,28 @@
+
+
+int led1 = D7; 
+
+int photoresistor = A0;
+int analogValue;
+
+
+void setup() {
+
+
+
+  pinMode(led1, OUTPUT);
+  pinMode(photoresistor, INPUT);
+
+}
+
+
+void loop() {
+ 
+analogValue = analogRead(photoresistor);
+String light = String(analogValue);
+Particle.publish("temp", light, PRIVATE);
+
+delay(30000);
+
+ 
+}
